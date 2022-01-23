8fee06d42d88cb47d33147393b74b518025c2f8f
ponta1106 <qqkz2qf9k@kind.ocn.ne.jp>
2022-01-23 22:52:57 +0900

    Add: canvasを使ったお絵かきアプリの作成

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="CL ball" src="./assets/turtle.png">
+    <DrawTool/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DrawTool from './components/DrawTool.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    DrawTool
   }
 }
 </script>
